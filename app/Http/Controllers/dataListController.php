779bbc4405c036fa005b38baf5de90b8318b826b
Mohan Dharavath <dharavath.mohan47@gmail.com>
2022-03-09 21:46:54 +0000

    Achievement of all tasks

--- app/Http/Controllers/dataListController.php
+++ app/Http/Controllers/dataListController.php
@@ -11,19 +11,13 @@ class dataListController extends Controller
 {
     public function displaydata()
     {
-        $filtereddata = [];
-        // $candidates = DB::table('candidates')->get();
-
         $candidates = DB::table('jobs')
+        ->join('candidates', 'candidates.id', '=', 'jobs.user_id')
         ->select('jobs.user_id','candidates.FirstName','candidates.LastName','candidates.Email',
         'jobs.jobTittle','jobs.companyName','jobs.startDate','jobs.endDate')
-        ->join('candidates', function ($join) {
-            $join->on('jobs.user_id', '=', 'candidates.id')
-            ->orderBy('jobs.endDate', 'DESC')
-            ->orderBy('jobs.user_id', 'ASC');
-        })
+        ->orderBy('jobs.user_id', 'ASC')
+        ->orderBy('jobs.endDate', 'DESC')
         ->get();
-
         return view('displayInfo',['data'=>$candidates]);
     }
 }
